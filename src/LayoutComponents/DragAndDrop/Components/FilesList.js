2767a6a6f97f905bedd6123224f3749863555edb
Roman <roman.dronov1@gmail.com>
2020-09-16 21:25:14 +0300

    drag and drop v1.0

--- src/LayoutComponents/DragAndDrop/Components/FilesList.js
+++ src/LayoutComponents/DragAndDrop/Components/FilesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './FilesList.css';
 export default class FilesList extends React.Component{
     getNames(files){
         let names=[];
@@ -7,10 +8,43 @@ export default class FilesList extends React.Component{
         }
         return names;
     }
+    getSize(file){
+        let size=file.size;
+        if(size<1000000){
+            return `${Math.round(size/1000)} KB`;
+        }
+        else if(size<1000000000){
+            size=size/1000000;
+            return `${Math.floor(size * 100) / 100} MB`;
+        }
+        else{
+            size=size/1000000000;
+            return `${Math.floor(size * 100) / 100} GB`;
+        }
+        
+    }
+    getName(file){
+        return file.name;
+        }
+    getFilesInformation(files){
+        let filesInfo=[];
+        for(let i=0;i<files.length;i++){
+            filesInfo.push(<div className='files'>
+                <div className='name' id={`f${i}`}>{this.getName(files[i])} {this.getSize(files[i])}</div>
+                <button className='deleteButton' onClick={()=>this.handeleDeleteFileClick(i)}>X</button>
+                </div>);
+        }
+        return filesInfo;
+    }
+    handeleDeleteFileClick(id){
+        this.props.deleteFile(id);
+    }
     render(){
-        let names=this.getNames(this.props.files);
-        return(
-            <div>{names}</div>
+        let files=this.getFilesInformation(this.props.files);
+        return(<div className="FilesList">
+            <div className="Title">Uploaded files</div>
+            <div>{files}</div></div>
+            
         )
     }
 }
--- src/LayoutComponents/DragAndDrop/DragAndDrop.js
+++ src/LayoutComponents/DragAndDrop/DragAndDrop.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import FilesList from './Components/FilesList';
 import './DragAndDrop.css';
+import ReleaseBox from './Components/ReleaseBox';
+import PreDropInfo from './Components/PreDropInfo';
 export default class DragAndDrop extends React.Component{
     constructor(props){
         super(props);
+        this.iconsSize=this.props.iconsSize;
         this.state={
             files:[],
+            release:false
         }
     }
     dragOver(e){
         e.preventDefault();
+        console.log('dragOver');
     }
     dragEnter(e){
         e.preventDefault();
+        console.log('dragEnter');
+        this.setState({release:true});
     }
     dragLeave(e){
         e.preventDefault();
+        console.log('dragLeave');
+        this.setState({release:false});
     }
     fileDrop(e){
         e.preventDefault();
@@ -26,17 +35,28 @@ export default class DragAndDrop extends React.Component{
             arrayFiles.push(files[i]);
         }
         console.log(arrayFiles);
-        this.setState({files:arrayFiles});
+        this.setState({files:arrayFiles,release:false});
+    }
+    deleteFile(id){
+        let files=this.state.files;
+        files.splice(id,1);
+        this.setState({
+            files:files
+        })
     }
     render(){
-        let list=this.state.files.length>0?<FilesList files={this.state.files}/>:null;
+        let list=this.state.files.length>0?<FilesList files={this.state.files} deleteFile={()=>this.deleteFile()}/>:null;
+        let dropInfo=this.state.release?<ReleaseBox size={this.iconsSize}/>:<PreDropInfo size={this.iconsSize}/>;
         return(
-            <div>
+            <div className='DragAndDrop'>
+                <div className='Title'>Upload photos</div>
                 <div className="dropContainer"
-                onDragOver={this.dragOver}
-                onDragEnter={this.dragEnter}
-                onDragLeave={this.dragLeave}
-                onDrop={(e)=>this.fileDrop(e)}></div>
+                onDragOver={(e)=>this.dragOver(e)}
+                onDragEnter={(e)=>this.dragEnter(e)}
+                onDragLeave={(e)=>this.dragLeave(e)}
+                onDrop={(e)=>this.fileDrop(e)}>
+                  {dropInfo}
+                </div>
                 {list}
             </div>
         )
